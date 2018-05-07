c1fc43c90318b0fd0469768ac229cbc9f94a6e49
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-05-07 15:27:33 +0200

    22.4

---
+++ server/routes/lane.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import * as LaneController from '../controllers/lane.controller';
+
+const router = new Router();
+
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Edit lane
+router.route('/lanes/:laneId').put(LaneController.editLane);
+
+// Delete Lane
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+export default router;
